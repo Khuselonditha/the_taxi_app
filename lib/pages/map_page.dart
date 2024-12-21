8c947a6d4672687281b9b2f4adea25c4353a5f10
khuselo <khuselonditha@gmail.com>
2024-12-21 19:29:26 +0200

    After adding map to the home screen.

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -8,8 +9,18 @@ class MapPage extends StatefulWidget {
 }
 
 class MapPageState extends State<MapPage> {
+  // Set the initial position of the map location
+  static const LatLng _myHouse = LatLng(-26.2365, 27.8422);
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: _myHouse,
+          zoom: 14,
+        ),
+      ),
+    );
   }
 }
